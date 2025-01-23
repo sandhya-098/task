1a29c92ee26e5a0b650cc7177740d9a625b4d61e
sandhya-098 <sandhya.r2023ece@sece.ac.in>
2025-01-23 15:15:46 +0530

    second commit

---
+++ portfolio/day5.js
@@ -0,0 +1,141 @@
+/*let age=26;
+if(age>=18){
+    console.log("eligible for vote");
+}
+else{
+    console.log("you are not eligible to vote");
+}
+    */
+/*let sandhya="e";
+switch(sandhya){
+    case 'a':
+        console.log("vowel");
+        break;
+        case 'e':
+            console.log("vowel");
+            break;
+            
+        case 'i':
+            console.log("vowel");
+            break;
+        case 'o':
+            console.log("vowel");
+            break;
+        case 'u':
+            console.log("vowel");
+            break;
+        default:
+            console.log("not a vowel");
+}
+            */
+/*let num="20";
+if(num>=0){
+    console.log("it is a positive number");
+}
+else{
+    console.log("it is a negative number");
+}
+*/
+/*let num = "30";
+if(num%2==0){
+console.log("even number");
+}
+else{
+console.log("odd number")
+}
+*/
+/* for(i=0;i<=50;i++){
+console.log(i)
+}
+*/let mark = prompt("enter mark");
+
+switch (true) { 
+    case (mark > 0 && mark <= 35):
+        console.log("fail");
+        break;
+    case (mark >= 36 && mark <= 50):
+        console.log("C grade");
+        break;
+    case (mark >= 51 && mark <= 70):
+        console.log("B grade");
+        break;
+    case (mark >= 71 && mark <= 90):
+        console.log("A grade");
+        break;
+    case (mark >= 91 && mark <= 100):
+        console.log("O grade");
+        break;
+    default:
+        console.log("Invalid marks");
+} 
+let mark=prompt("enter mark");
+if (mark > 0 && mark <= 35){
+    console.log("fail");
+}
+else if(mark >= 36 && mark <= 50){
+    console.log("C grade");
+}
+else if(mark >= 51 && mark <= 70){
+    console.log("B grade");
+}
+else if(mark >= 71 && mark <= 90){
+    console.log("A grade");
+}
+else if(mark >= 91 && mark <= 100){
+    console.log("O grade");
+}
+else{
+    console.log("Invalid marks");
+}
+    //afternoon
+for(i=0;i<50;i++){
+    console.log(i);
+}
+for (i=0;i<=10;i++){
+     console.log("5 * "+i+" = "+ 5*i);
+}
+     
+for(i=50;i>0;i--){
+    console.log(i);
+}
+let a=0;
+for(let i=0;i<=50;i++){
+    
+    a+=i;
+    
+}
+console.log(a);
+
+
+*/
+
+
+for (i=0;i<=5;i++){
+    let star=" ";
+    for(j=0;j<=i;j++){
+        star +="*";
+    }
+    console.log(star);
+}
+function add(a,b){
+    return a+b;
+}
+c=add(10,50);
+console.log(c);
+}
+*/
+let abc=(x,y)=console(x+y);
+abc(10,50);
+*/
+function abc(name,company){
+    console.log("i'm "+name+" glared to meet you in this fest. i'm a chief developer at "+company);
+}
+abc(sandhya,microsoft);
+*/
+
+                            
+
+
+
+
+
